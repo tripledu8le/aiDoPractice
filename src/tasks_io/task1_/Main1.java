4366fde88126ec6f3647e269a5adf1df8ed7eabe
itdep <morozvitali99@gmail.com>
2025-12-19 11:41:42 +0200

    task io 1 ### 01 of 10

---
+++ src/tasks_io/task1_/Main1.java
@@ -0,0 +1,22 @@
+package tasks_io.task1_;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main1 {
+    public void practice1() {
+        File file = new File("data/example/txt");
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File created");
+            } else {
+                System.out.println("File already exists");
+            }
+        } catch (IOException e) {
+            System.out.println("Eroor creating file " + e.getMessage());
+        }
+    }
+
+
+
+}
